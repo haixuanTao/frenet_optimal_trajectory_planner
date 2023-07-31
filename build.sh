288e9dfe2059120fff4fc00529d0feda808f5606
haixuanTao <tao.xavier@outlook.com>
2023-07-31 22:19:39 +0200

    Make setup py pip distributable

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt-get install -y libeigen3-dev clang cmake
-if [ ! -d "build" ]; then
-  mkdir build
+if [ -d "build" ]; then
+  rm -rf build dist
 fi
+mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --target all -- -j 8
--- setup.py
+++ setup.py
@@ -19,46 +19,55 @@ class BinaryDistribution(Distribution):
 
 
 def main():
-    os.environ['CC'] = 'g++'
-    os.environ['CXX'] = 'g++'
     pwd = os.path.dirname(os.path.realpath(__file__))
-
+    os.system("./build.sh")
+    os.environ["CC"] = "g++"
+    os.environ["CXX"] = "g++"
     # Path to Eigen might differ, replace path if necessary
     CFLAGS = [
-        '-std=c++11', '-fPIC', '-shared', '-I', '-O3', '-pthread', '-Wall',
-        '-I/usr/include/eigen3'
+        "-std=c++11",
+        "-fPIC",
+        "-shared",
+        "-I",
+        "-O3",
+        "-pthread",
+        "-Wall",
+        "-I/usr/include/eigen3",
     ]
 
     LDFLAGS = []
-    LDFLAGS += ['-Xlinker', '-export-dynamic']
-    LDFLAGS += ['-W','-Wno-undef', '-lstdc++', '-static-libstdc++']
+    LDFLAGS += ["-Xlinker", "-export-dynamic"]
+    LDFLAGS += ["-W", "-Wno-undef", "-lstdc++", "-static-libstdc++"]
 
     # Link static library libPackageFrenetOptimalTrajectory.a
-    LDFLAGS += [
-        '-L' + os.path.join(pwd, 'build'), '-lPackageFrenetOptimalTrajectory'
-    ]
-    LDFLAGS += [
-        '-I', '/usr/include/x86_64-linux-gnu/qt5/QtCore', '-l', 'Qt5Core'
-    ]
+    LDFLAGS += ["-L" + os.path.join(pwd, "build"), "-lPackageFrenetOptimalTrajectory"]
+    LDFLAGS += ["-I", "/usr/include/x86_64-linux-gnu/qt5/QtCore", "-l", "Qt5Core"]
     print("Linker Flag:", LDFLAGS)
 
     module = Extension(
-        'fot_planner',
-        sources=['src/FrenetOptimalTrajectory/planner_package.cpp'],
-        language='C++',
+        "fot_planner",
+        sources=["src/FrenetOptimalTrajectory/planner_package.cpp"],
+        language="C++",
         include_dirs=[
-            'src', 'src/CubicSpline', 'src/Polynomials',
-            'src/FrenetOptimalTrajectory', 'src/Obstacle', 'src/Car'
+            "src",
+            "src/CubicSpline",
+            "src/Polynomials",
+            "src/FrenetOptimalTrajectory",
+            "src/Obstacle",
+            "src/Car",
         ],
         extra_compile_args=CFLAGS,
-        extra_link_args=LDFLAGS)
+        extra_link_args=LDFLAGS,
+    )
 
-    setup(name="fot_planner",
-          version="1.0.0",
-          description="FOT Planner",
-          author="ERDOS Project",
-          url="https://github.com/erdos-project/",
-          ext_modules=[module])
+    setup(
+        name="fot_planner",
+        version="1.0.0",
+        description="FOT Planner",
+        author="ERDOS Project",
+        url="https://github.com/erdos-project/",
+        ext_modules=[module],
+    )
 
 
 if __name__ == "__main__":
